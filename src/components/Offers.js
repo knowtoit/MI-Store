823bf491abe123c51ad410db6483dc9b1f5ad653
gourav mittal <107996221+gouravmittal8081@users.noreply.github.com>
2022-11-27 20:58:42 +0530

    Update Offers.js

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -6,9 +6,11 @@ import "../styles/Offers.css"
 const Offers = ({offer}) => {
     return (
         <div className="offersSection"> 
-        {offer.map((item,index)=>(
+        {
+        offer.map((item,index)=>(
         <Offer key={item.image} index={index} src={item.image} link={item.url} />
-        ))}
+        ))
+        }
     </div>
     )
 }
