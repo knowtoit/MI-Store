2e449fa91df21c66c3806a88b51cfa33deca7432
gourav mittal <107996221+gouravmittal8081@users.noreply.github.com>
2022-11-27 20:44:38 +0530

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,17 +6,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Slider = ({start}) => {
     return (
         <Carousel fade>
-       
-
-       {start.map((item)=>(
+    
+       {
+       start.map((item)=>(
             <Carousel.Item>
-            <img
+            <img 
               className="d-block w-100"
               src={item}
               alt="First slide"
             />
           </Carousel.Item>
-       ))}
+       ))
+       }
        
       </Carousel>
     )
